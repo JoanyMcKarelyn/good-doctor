24793f6ac69afa51de3df6fc040c2047c08a9615
JoanyMcKarelyn <109032964+JoanyMcKarelyn@users.noreply.github.com>
2022-12-03 16:55:17 +0800

    v1.1
    - pacify cliff racers

--- MWSE/mods/Spammer/Good Doctor/main.lua
+++ MWSE/mods/Spammer/Good Doctor/main.lua
@@ -1,115 +1,90 @@
-local mod = {name = "Good Doctor (Joseph Edit)", ver = "1.0"}
-local config = require("Spammer.Good Doctor.config").config
-local logger = require("Spammer.Good Doctor.logging").createLogger("main")
+local mod = "Good Doctor (Joseph Edit)"
+local configPath = mod
+local defaultConfig = {
+	peaceful = { ["cliff racer"] = true, ["guar"] = true, ["shalk"] = true },
+	pacifyDiseased = {
+		["cliff racer_blighted"] = true,
+		["shalk_diseased"] = true,
+		["shalk_diseased_hram"] = true,
+		["guar_feral"] = true,
+	},
+	pacifyBlighted = { ["cliff racer_blighted"] = true, ["shalk_blighted"] = true },
+}
+
+local config = mwse.loadConfig(configPath, defaultConfig)
 
 ---@param e table|spellResistEventData
 event.register("spellResist", function(e)
-    if (e.effect.id == tes3.effect.cureCommonDisease and
-        config.pacifyDiseased[e.target.baseObject.id:lower()]) or
-        (e.effect.id == tes3.effect.cureBlightDisease and
-            config.pacifyBlighted[e.target.baseObject.id:lower()]) then
-        e.target.mobile.fight = 0
-        logger:debug("%s's fight value is now: %s", e.target.id,
-                     e.target.mobile.fight)
-        e.target.mobile.flee = 100
-        e.target.mobile.actionData.aiBehaviorState = tes3.aiBehaviorState.flee
-        e.target.mobile:stopCombat(true)
-    end
+	if (e.effect.id == tes3.effect.cureCommonDisease and config.pacifyDiseased[e.target.baseObject.id:lower()]) or
+	(e.effect.id == tes3.effect.cureBlightDisease and config.pacifyBlighted[e.target.baseObject.id:lower()]) then
+		e.target.mobile.fight = 0
+		e.target.mobile.flee = 100
+		e.target.mobile.actionData.aiBehaviorState = tes3.aiBehaviorState.flee
+		e.target.mobile:stopCombat(true)
+	end
 end)
 
 local function onInitialized()
-    for creature in tes3.iterateObjects(tes3.objectType.creature) do
-        if config.peaceful[creature.id] then creature.aiConfig.fight = 0 end
-    end
+	for creature in tes3.iterateObjects(tes3.objectType.creature) do
+		if config.peaceful[creature.id] then
+			creature.aiConfig.fight = 0
+		end
+	end
 end
 event.register("initialized", onInitialized)
 
 local function registerModConfig()
-    local template = mwse.mcm.createTemplate(mod.name)
-    template.onClose = function() config.save() end
-    template:register()
+	local template = mwse.mcm.createTemplate(mod)
+	template.onClose = function()
+		config.save()
+	end
+	template:register()
 
-    local page =
-        template:createPage({label = "\"" .. mod.name .. "\" Settings"})
-    page:createInfo{
-        text = "Welcome to \"" .. mod.name .. "\" Configuration Menu. \n \n \n" ..
-            "Original mod by Spammer, edited by JosephMcKean."
-    }
-    page:createHyperLink{
-        text = "Spammer's Nexus Profile",
-        url = "https://www.nexusmods.com/users/140139148?tab=user+files"
-    }
-    page:createDropdown{
-        label = "Log Level",
-        description = "Set the logging level.",
-        options = {
-            {label = "DEBUG", value = "DEBUG"},
-            {label = "INFO", value = "INFO"},
-            {label = "ERROR", value = "ERROR"}, {label = "NONE", value = "NONE"}
-        },
-        variable = mwse.mcm.createTableVariable {
-            id = "logLevel",
-            table = config
-        },
-        -- code that copied over from merlord's mod
-        callback = function(self)
-            for _, log in ipairs(require("Spammer.Good Doctor.logging").loggers) do
-                mwse.log("Setting %s to log level %s", log.name,
-                         self.variable.value)
-                log:setLogLevel(self.variable.value)
-            end
-        end
-    }
-    local function createCreatureList()
-        local creatures = {}
-        for creature in tes3.iterateObjects(tes3.objectType.creature) do
-            if not (creature.baseObject and creature.baseObject.id ~=
-                creature.id) then
-                creatures[#creatures + 1] =
-                    (creature.baseObject or creature).id:lower()
-            end
-        end
-        table.sort(creatures)
-        return creatures
-    end
-    template:createExclusionsPage{
-        label = "Peaceful Creatures",
-        description = "Move creatures into the left list to allow them to be peaceful." ..
-            "\n" .. "\n" ..
-            "Requires a RESTART and does not affect references that are already created.",
-        variable = mwse.mcm.createTableVariable {
-            id = "peaceful",
-            table = config
-        },
-        leftListLabel = "Peaceful Creatures",
-        rightListLabel = "Creatures",
-        filters = {{label = "Creatures", callback = createCreatureList}}
-    }
-    template:createExclusionsPage{
-        label = "Pacified by Cure Common Disease",
-        description = "Move creatures into the left list to allow them to be pacified by Cure Common Disease spell." ..
-            "\n" .. "\n" ..
-            "Does not affect references that are already created.",
-        variable = mwse.mcm.createTableVariable {
-            id = "pacifyDiseased",
-            table = config
-        },
-        leftListLabel = "Creatures that can be pacified by Cure Common Disease spell",
-        rightListLabel = "Creatures",
-        filters = {{label = "Creatures", callback = createCreatureList}}
-    }
-    template:createExclusionsPage{
-        label = "Pacified by Cure Blight Disease",
-        description = "Move creatures into the left list to allow them to be pacified by Cure Blight Disease spell." ..
-            "\n" .. "\n" ..
-            "Does not affect references that are already created.",
-        variable = mwse.mcm.createTableVariable {
-            id = "pacifyBlighted",
-            table = config
-        },
-        leftListLabel = "Creatures that can be pacified by Cure Blight Disease spell",
-        rightListLabel = "Creatures",
-        filters = {{label = "Creatures", callback = createCreatureList}}
-    }
+	local page = template:createPage({ label = "\"" .. mod .. "\" Settings" })
+	page:createInfo{
+		text = "Welcome to \"" .. mod .. "\" Configuration Menu. \n \n \n" ..
+		"Original mod by Spammer, edited by JosephMcKean.",
+	}
+	page:createHyperLink{
+		text = "Spammer's Nexus Profile",
+		url = "https://www.nexusmods.com/users/140139148?tab=user+files",
+	}
+	local function createCreatureList()
+		local creatures = {}
+		for creature in tes3.iterateObjects(tes3.objectType.creature) do
+			if not (creature.baseObject and creature.baseObject.id ~= creature.id) then
+				creatures[#creatures + 1] = (creature.baseObject or creature).id:lower()
+			end
+		end
+		table.sort(creatures)
+		return creatures
+	end
+	template:createExclusionsPage{
+		label = "Peaceful Creatures",
+		description = "Move creatures into the left list to allow them to be peaceful." .. "\n" .. "\n" ..
+		"Requires a RESTART and does not affect references that are already created.",
+		variable = mwse.mcm.createTableVariable { id = "peaceful", table = config },
+		leftListLabel = "Peaceful Creatures",
+		rightListLabel = "Creatures",
+		filters = { { label = "Creatures", callback = createCreatureList } },
+	}
+	template:createExclusionsPage{
+		label = "Pacified by Cure Common Disease",
+		description = "Move creatures into the left list to allow them to be pacified by Cure Common Disease spell." .. "\n" ..
+		"\n" .. "Does not affect references that are already created.",
+		variable = mwse.mcm.createTableVariable { id = "pacifyDiseased", table = config },
+		leftListLabel = "Creatures that can be pacified by Cure Common Disease spell",
+		rightListLabel = "Creatures",
+		filters = { { label = "Creatures", callback = createCreatureList } },
+	}
+	template:createExclusionsPage{
+		label = "Pacified by Cure Blight Disease",
+		description = "Move creatures into the left list to allow them to be pacified by Cure Blight Disease spell." .. "\n" ..
+		"\n" .. "Does not affect references that are already created.",
+		variable = mwse.mcm.createTableVariable { id = "pacifyBlighted", table = config },
+		leftListLabel = "Creatures that can be pacified by Cure Blight Disease spell",
+		rightListLabel = "Creatures",
+		filters = { { label = "Creatures", callback = createCreatureList } },
+	}
 end
 event.register("modConfigReady", registerModConfig)
